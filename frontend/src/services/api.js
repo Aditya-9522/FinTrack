f48377614beb7d4f756e3887bc71d1ff810e4afe
Aditya Vardhan <adityavardhanp09@gmail.com>
2026-02-28 08:56:35 +0530

    sixth commit

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://fintrack-cagy.onrender.com"
+  baseURL: "https://fintrack-cagy.onrender.com/api"
 });
 
 // Attach token automatically to every request
